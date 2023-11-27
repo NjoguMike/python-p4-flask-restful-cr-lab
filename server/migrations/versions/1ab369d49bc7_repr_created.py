74abf739edfd81144e50f8293375527ae649c90b
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-27 10:30:21 +0300

    Complete Restful Lab

---
+++ server/migrations/versions/1ab369d49bc7_repr_created.py
@@ -0,0 +1,24 @@
+"""Repr created
+
+Revision ID: 1ab369d49bc7
+Revises: 3065d53fb38f
+Create Date: 2023-11-27 09:57:48.935546
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1ab369d49bc7'
+down_revision = '3065d53fb38f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ server/migrations/versions/3065d53fb38f_updated_model.py
@@ -0,0 +1,24 @@
+"""Updated Model
+
+Revision ID: 3065d53fb38f
+Revises: 67f5d67aea55
+Create Date: 2023-11-27 09:55:31.553039
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3065d53fb38f'
+down_revision = '67f5d67aea55'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
